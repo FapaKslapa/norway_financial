2faa75011726629b5593c78366d09164d3997c57
fapakslapa <stefanomarocco0@gmail.com>
2026-05-27 13:46:39 +0200

    feat: update database schemas and add new migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
-import { env } from "../env";
+import { env } from "@/env";
 import * as schema from "./schema";
 
 const poolConnection = mysql.createPool({
